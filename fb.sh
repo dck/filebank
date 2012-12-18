9084625cb05aa322a7e96fe2d293c05d43706f6d
Denis Usanov <usanovdd@gmail.com>
2012-12-18 14:33:01 +0400

    Restore command was implementated

--- fb.sh
+++ fb.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 BANKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.bank"
-HISTORY=$BANKDIR/history
+# HISTORY=history
+# HISTORYPATH=$BANKDIR/$HISTORY
+MAXSIZE=$((10*1024*1024))  # bytes
 
 if [ ! -e $BANKDIR ]
 then
     mkdir $BANKDIR
 fi
 
-if [ ! -e $HISTORY ]
-then
-    touch $HISTORY
-fi
+# if [ ! -e $HISTORYPATH ]
+# then
+#     touch $HISTORYPATH
+# fi
 
-MAXSIZE=$((10*1024*1024)) # bytes
+
 
 remove()
 {
@@ -34,7 +36,7 @@ remove()
             else
                 abs=$(readlink -f $i)
                 mv $i $BANKDIR/$i
-                echo "$i $abs" >> $HISTORY
+                # echo "$i $abs" >> $HISTORYPATH
             fi
         fi
 
@@ -42,13 +44,31 @@ remove()
 }
 restore()
 {
-    echo "restore act."
+    for i in $@
+    do
+        if [ -e $BANKDIR/$i ];
+        then
+            mv $BANKDIR/$i `pwd`/$i
+        else
+            echo "File $i not found in bank"
+        fi
+
+    done
 }
 
 out()
 {
-    echo "out act."
+    for i in $@
+    do
+        rm $i
+    done
 }
+
+list()
+{
+    ls -lh $BANKDIR | awk 'NF>2' | awk '{printf "%-20s %5s\n",$9, $5}'
+}
+
 usage()
 {
     echo "Usage: $0 <rm|restore|out> file1[,file2[,file3[,...]]]"
@@ -70,6 +90,9 @@ case "$1" in
     ;;
 "out")
     out ${@:2}
+    ;;
+"list")
+    list ${@:2}
     ;;
 *)
     usage
